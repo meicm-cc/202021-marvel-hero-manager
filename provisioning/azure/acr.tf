f330fdfc5b3d774dde422175965fb42f69d2f463
Ricardo Gomes <ricardo@bytencode.com>
2021-06-04 19:55:44 +0100

    fixes

--- provisioning/azure/acr.tf
+++ provisioning/azure/acr.tf
@@ -6,14 +6,6 @@ resource "azurerm_container_registry" "meicm" {
   admin_enabled            = false
 }
 
-
-resource "azurerm_role_assignment" "aks_acr" {
-  scope                = azurerm_container_registry.meicm.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.meicm.kubelet_identity[0].object_id
-}
-
-
 output "azure_container_server" {
   value = azurerm_container_registry.meicm.login_server
 }
